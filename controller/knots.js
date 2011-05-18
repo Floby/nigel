d0409b54570d224043a2a15b7739a1a568378c41
Florent Jaby <florent.jaby@gmail.com>
2011-05-18 16:55:13 +0200

    list knots and show single knot in a web page

--- controller/knots.js
+++ controller/knots.js
@@ -2,9 +2,25 @@ var util = require('util');
 var Knot = require('../lib/FsKnot');
 
 module.exports = function knots(req, res, next) {
-    res.writeHead(200, {
-        "content-type": "text/plain"
+    if(req.params.id) return showKnot(req, res, next);
+    return listKnots(req, res, next);
+}
+
+function listKnots (req, res, next) {
+    Knot.list(function(err, knots) {
+        if(err) return next(err);
+        res.write('showing '+knots.length+' knots\n');
+        knots.forEach(function(k) {
+            res.write(k._id+'\n');
+        })
+        res.end();
     });
-    res.write(util.inspect(req));
-    res.end("\nyou've got yourself a knot ;)\n");
+}
+
+function showKnot (req, res, next) {
+    Knot.get(req.params.id, function(err, k) {
+       //if(err) return next(err);
+        res.write(util.inspect(k));
+        res.end('\n');
+    })
 }
