7ee68c4c3e761188a17533c703ccc0fc3ac4cdba
Florent Jaby <florent.jaby@gmail.com>
2011-10-05 18:40:02 +0200

    try a zend style routing with module/controller/action

--- controller/setup.js
+++ controller/setup.js
@@ -1,4 +1,5 @@
 var util = require('util');
+var express = require('express');
 
 function controller (name) {
     var ctl;
@@ -12,6 +13,21 @@ function controller (name) {
     return ctl;
 }
 
+function parseArgs (req, res, next) {
+    var chain = req.params[0];
+    if(!chain) return next();
+    var split = chain.split('/');
+    for (var i = 0; i < split.length; i=i+2) {
+        var value = split[i+1] || true;
+        req.params[split[i]] = value;
+    }
+    next();
+}
+
+function autoRoute (req, res, next) {
+    return controller(req.params.controller)(req, res, next);
+}
+
 exports.setup_routes = function setup_routes (app) {
     app.get('/', function(req, res){
       res.render('index', {
@@ -20,4 +36,10 @@ exports.setup_routes = function setup_routes (app) {
     });
 
     app.get('/knots/:id?/:action?', controller('knots'));
+    app.get('/:module/:controller/:action/*', parseArgs, autoRoute, function(req, res, next) {
+        res.header('content-type', 'text/plain');
+        res.write(util.inspect(req.params));
+        res.write('\n');
+        res.end();
+    })
 }
